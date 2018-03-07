b36b9a4cffe35ae369eda0df1670556ba4a7d240
TJ Horner <me@tjhorner.com>
2018-03-06 23:46:57 -0800

    searching and use graceful-fs

--- concurrent.js
+++ concurrent.js
@@ -1,6 +1,6 @@
 const ProgressBar = require('ascii-progress')
 const request = require('request')
-const fs = require('fs')
+const fs = require('graceful-fs')
 
 require('colors')
 
@@ -13,14 +13,9 @@ const maxConcurrentDownloads = argv["max-concurrent"] || argv["c"] || 5
 
 const useIds = argv["use-ids"] ? true : false
 
+const ua = "e621 id downloader (https://github.com/tetra-fox/e621-id-downloader)"
+
 let ids
-
-if (!fs.existsSync(dlPath)) fs.mkdirSync(dlPath)
-
-if (!fs.existsSync(idsPath)) {
-  console.log(`file "${idsPath}" does not exist! aborting.`)
-  process.exit(1)
-}
 
 var queue = new Array(maxConcurrentDownloads).fill(null)
 var progressBars = new Array(maxConcurrentDownloads).fill(null).map(() => new ProgressBar({ width: 20, clear: true }))
@@ -33,17 +28,52 @@ var totalBar = new ProgressBar({
 
 var currentIndex = 0
 
-fs.readFile(idsPath, "utf8", (err, contents) => {
-  ids = contents.split(",")
-  console.log(`downloading ${ids.length} files...`.yellow)
-  totalBar.total = ids.length
-  totalBar.tick(0)
-  console.log("")
+if(argv["s"] || argv["search"]) {
+  var tags = argv["s"] || argv["search"]
+  var limit = argv["l"] || argv["limit"] || 10
+  console.log(`searching for ${tags}`.yellow)
 
-  // fill all spots initially
-  for(var i = 0; i < maxConcurrentDownloads; i++)
-    download()
-})
+  // TODO save image urls for later instead of downloading
+  // them again
+  request("https://e621.net/post/index.json", {
+    qs: {
+      tags, limit
+    },
+    headers: {
+      "User-Agent": ua
+    }, json: true
+  }, (err, res, body) => {
+    ids = body.map(i => i.id)
+
+    console.log(`downloading ${ids.length} files...`.yellow)
+    totalBar.total = ids.length
+    totalBar.tick(0)
+    console.log("")
+  
+    // fill all spots initially
+    for(var i = 0; i < maxConcurrentDownloads; i++)
+      download()
+  })
+} else {
+  if (!fs.existsSync(dlPath)) fs.mkdirSync(dlPath)
+  
+  if (!fs.existsSync(idsPath)) {
+    console.log(`file "${idsPath}" does not exist! aborting.`)
+    process.exit(1)
+  }
+  
+  fs.readFile(idsPath, "utf8", (err, contents) => {
+    ids = contents.split(",")
+    console.log(`downloading ${ids.length} files...`.yellow)
+    totalBar.total = ids.length
+    totalBar.tick(0)
+    console.log("")
+  
+    // fill all spots initially
+    for(var i = 0; i < maxConcurrentDownloads; i++)
+      download()
+  })
+}
 
 var download = () => {
   // do we have a spot in the queue open?
@@ -58,10 +88,10 @@ var download = () => {
     if(id !== undefined) {
       request(`https://e621.net/post/show.json?id=${id}`, {
         headers: {
-          "User-Agent": "e621 id downloader (https://github.com/tetra-fox/e621-id-downloader)"
+          "User-Agent": ua
         }, json: true
       }, (err, res, body) => {
-        if(body.md5) {
+        if(!err && res.statusCode === 200 && body && body.md5) {
           var filename = body.file_url.substring(body.file_url.lastIndexOf("/") + 1)
           var fileExt = filename.split(".")[1]
           
@@ -75,20 +105,26 @@ var download = () => {
   
           bar.setSchema(`[:bar] ${useIds ? `${id}.${fileExt}` : filename} :percent, :etas left (${thisIndex + 1}/${ids.length})`.cyan, true)
     
-          request(body.file_url)
+          var stream = fs.createWriteStream(`${dlPath}/${useIds ? `${id}.${fileExt}` : filename}`)
+
+          var fileDl = request(body.file_url)
             .on("response", res => {
               bar.total = parseInt(res.headers["content-length"], 10)
   
               res.on("data", data => {
                 bar.tick(data.length)
               })
+
+              res.on("end", () => {
+                // need to do this to avoid reaching OS-defined
+                // hard limit for open files/connections (EMFILE)
+                stream.close()
+                totalBar.tick()
+                queue[openSpot] = null
+                download()
+              })
             })
-            .pipe(fs.createWriteStream(`${dlPath}/${useIds ? `${id}.${fileExt}` : filename}`))
-            .on("finish", () => {
-              totalBar.tick()
-              queue[openSpot] = null
-              download()
-            })
+            .pipe(stream)
         } else {
           var bar = new ProgressBar({
             width: 20,
